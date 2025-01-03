445013686ae34ec4579e90b9aa5e60d5170a605d
Danial Kurtumerov <kur7umerovd@gmail.com>
2025-01-03 15:57:36 +0500

    tests: introduction
    Changed tests for introduction a bit.

--- tests/test_introduction.py
+++ tests/test_introduction.py
@@ -1,15 +1,14 @@
 """Test introduction module."""
 
-from langchain_core.messages import AIMessage, BaseMessage
+from langchain_core.messages import BaseMessage
 
 from src.introduction import chat, message
 
 
-def test_chat_ollama_invoke() -> None:
+def test_invoke() -> None:
     """Test ChatOllama invoke method."""
     response: BaseMessage = chat.invoke(message)
 
-    assert isinstance(response, AIMessage)
     assert response.content == (
         "Hello! It's nice to meet you. " +
         "Is there something I can help you with or would you like to chat?"

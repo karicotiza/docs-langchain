05421981ff019dcbc32bb84d01a9f966136ee393
Danial Kurtumerov <kur7umerovd@gmail.com>
2025-01-03 15:27:23 +0500

    chore: introduction
    Made introduction more similar to reference guide.

--- src/introduction.py
+++ src/introduction.py
@@ -5,3 +5,4 @@ from langchain_ollama import ChatOllama
 from src.settings import model, temperature
 
 chat: ChatOllama = ChatOllama(model=model, temperature=temperature)
+message: str = 'Hello, world!'
--- tests/test_introduction.py
+++ tests/test_introduction.py
@@ -2,12 +2,15 @@
 
 from langchain_core.messages import AIMessage, BaseMessage
 
-from src.introduction import chat
+from src.introduction import chat, message
 
 
 def test_chat_ollama_invoke() -> None:
     """Test ChatOllama invoke method."""
-    response: BaseMessage = chat.invoke('Hi')
+    response: BaseMessage = chat.invoke(message)
 
     assert isinstance(response, AIMessage)
-    assert response.content == 'How can I assist you today?'
+    assert response.content == (
+        "Hello! It's nice to meet you. " +
+        "Is there something I can help you with or would you like to chat?"
+    )

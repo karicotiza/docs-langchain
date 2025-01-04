01049d76719e9ee2c657f30e7872f93269b475ef
Danial Kurtumerov <kur7umerovd@gmail.com>
2025-01-04 10:11:06 +0500

    feat: build a semantic search engine
    Updated code for "build_a_semantic_search_engine"
    page.

--- tests/tutorials/test_build_a_semantic_search_engine.py
+++ tests/tutorials/test_build_a_semantic_search_engine.py
@@ -8,3 +8,25 @@ def test_docs_length() -> None:
     reference_length: int = 107
 
     assert len(docs) == reference_length
+
+
+def test_docs_data() -> None:
+    size_of_slice: int = 200
+    reference_content: str = ''.join((
+        'Table of Contents\n',
+        'UNITED STATES\n',
+        'SECURITIES AND EXCHANGE COMMISSION\n',
+        'Washington, D.C. 20549\n',
+        'FORM 10-K\n',
+        '(Mark One)\n',
+        '☑  ANNUAL REPORT PURSUANT TO SECTION 13 OR 15(D) OF THE SECURITIES ',
+        'EXCHANGE ACT OF 1934\n',
+        'F',
+    ))
+
+    reference_metadata: dict[str, int | str] = {
+        'page': 0, 'source': 'data/nke-10k-2023.pdf',
+    }
+
+    assert docs[0].page_content[:size_of_slice] == reference_content
+    assert docs[0].metadata == reference_metadata

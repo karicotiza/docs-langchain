3de09fd275227e775af9cd8315f01fb2daaceb88
Danial Kurtumerov <kur7umerovd@gmail.com>
2025-01-31 15:34:32 +0500

    feat: build a chatbot.
    Updated code for "build a chatbot" page.

--- tests/tutorials/test_build_a_chatbot.py
+++ tests/tutorials/test_build_a_chatbot.py
@@ -5,10 +5,15 @@ Build a chatbot.
 
 from typing import Any
 
+import pytest
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
 from langchain_core.runnables import RunnableConfig
 
 from src.tutorials.build_a_chatbot import app, chat
+
+pytest_plugins: tuple[str, ...] = (
+    "pytest_asyncio",
+)
 
 
 def test_model_without_memory() -> None:
@@ -105,6 +110,73 @@ def test_app_memory() -> None:
     )
 
     response_4: dict[str, Any] | Any = app.invoke(
+        {
+            "messages": [HumanMessage("What's my name?")],
+        },
+        config_1,
+    )
+
+    assert response_4["messages"][-1].content == (
+        """You didn't tell me your last name, just "Bob". """
+        "I don't have any additional information about you beyond that. "
+        "Would you like to share it with me?"
+    )
+
+
+@pytest.mark.asyncio
+async def test_app_memory_async() -> None:
+    """Test app's memory async."""
+    config_1: RunnableConfig = RunnableConfig(
+        {
+            "configurable": {"thread_id": "abc111"},
+        },
+    )
+
+    config_2: RunnableConfig = RunnableConfig(
+        {
+            "configurable": {"thread_id": "abc222"},
+        },
+    )
+
+    response_1: dict[str, Any] | Any = await app.ainvoke(
+        {
+            "messages": [HumanMessage("Hi! I'm Bob.")],
+        },
+        config_1,
+    )
+
+    assert response_1["messages"][-1].content == (
+        "Hello Bob! It's nice to meet you. "
+        "Is there something I can help you with, or would you like to chat?"
+    )
+
+    response_2: dict[str, Any] | Any = await app.ainvoke(
+        {
+            "messages": [HumanMessage("What's my name?")],
+        },
+        config_1,
+    )
+
+    assert response_2["messages"][-1].content == (
+        "Your name is Bob. You told me that earlier."
+    )
+
+    response_3: dict[str, Any] | Any = await app.ainvoke(
+        {
+            "messages": [HumanMessage("What's my name?")],
+        },
+        config_2,
+    )
+
+    assert response_3["messages"][-1].content == (
+        "I don't have any information about your identity, "
+        "so I'm not aware of your name. We just started our conversation, "
+        "and I don't retain any data about individual users. "
+        "If you'd like to share your name with me, "
+        "I'd be happy to chat with you!"
+    )
+
+    response_4: dict[str, Any] | Any = await app.ainvoke(
         {
             "messages": [HumanMessage("What's my name?")],
         },

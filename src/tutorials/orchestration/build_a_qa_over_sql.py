b5111bc896f6ec61019f148e7ba19dbd30b53cab
Danial Kurtumerov <kur7umerovd@gmail.com>
2025-02-06 15:21:17 +0500

    feat: build a q/a over sql
    Added code for "build a question/answering system
    over sql data" page.

---
+++ src/tutorials/orchestration/build_a_qa_over_sql.py
@@ -0,0 +1,91 @@
+"""Tutorial module.
+
+Build a Question/Answering system over SQL data.
+
+Notes:
+* Looks like structured output can't handle SQL queries.
+
+"""
+from typing import TYPE_CHECKING, Annotated, Any, TypedDict
+
+from langchain import hub
+from langchain_community.tools.sql_database.tool import QuerySQLDatabaseTool
+from langchain_community.utilities import SQLDatabase
+from langchain_ollama import ChatOllama, OllamaLLM
+
+from src.settings import llm_model_name, llm_model_temperature, llm_model_url
+
+if TYPE_CHECKING:
+    from langchain_core.runnables import Runnable
+
+db: SQLDatabase = SQLDatabase.from_uri("sqlite:///data/Chinook.db")
+
+
+class State(TypedDict):
+    """State structure."""
+
+    question: str
+    query: str
+    result: str  # noqa: WPS
+    answer: str
+
+
+chat: ChatOllama = ChatOllama(
+    base_url=llm_model_url,
+    model=llm_model_name,
+    temperature=llm_model_temperature,
+)
+
+llm: OllamaLLM = OllamaLLM(
+    base_url=llm_model_url,
+    model=llm_model_name,
+    temperature=llm_model_temperature,
+)
+
+query_prompt_template: Any = hub.pull("langchain-ai/sql-query-system-prompt")
+
+
+# class QueryOutput(TypedDict):
+#     """Generated SQL query."""
+
+#     query: Annotated[str, ..., "Syntactically valid SQL query."]
+
+
+def write_query(state: State) -> dict[str, Any]:
+    """Generate SQL query to fetch information.
+
+    Args:
+        state (State): state structure.
+
+    Returns:
+        dict[str, Any]: dict with response.
+
+    """
+    prompt = query_prompt_template.invoke(
+        {
+            "dialect": db.dialect,
+            "top_k": 10,
+            "table_info": db.get_table_info(),
+            "input": state["question"],
+        },
+    )
+
+    # structured_llm: Runnable = chat.with_structured_output(QueryOutput)
+    # result: Any = structured_llm.invoke(prompt)  # noqa: WPS
+    # return {"query": result["query"]}
+    result: Any = llm.invoke(prompt)  # noqa: WPS
+    return {"query": result}
+
+
+def execute_query(state: State) -> dict[str, Any]:
+    """Execute SQL query.
+
+    Args:
+        state (State): state structure.
+
+    Returns:
+        dict[str, Any]: dict with response.
+
+    """
+    execute_query_tool: QuerySQLDatabaseTool = QuerySQLDatabaseTool(db=db)
+    return {"result": execute_query_tool.invoke(state["query"])}
---
+++ tests/tutorials/orchestration/test_build_a_qa_over_sql.py
@@ -0,0 +1,82 @@
+"""Test module.
+
+Build a Question/Answering system over SQL data.
+"""
+
+from typing import Any
+
+from src.tutorials.orchestration.build_a_qa_over_sql import (
+    db,
+    execute_query,
+    query_prompt_template,
+    write_query,
+)
+
+
+def test_db() -> None:
+    """Test database."""
+    tables: list[str] = [
+        "Album",
+        "Artist",
+        "Customer",
+        "Employee",
+        "Genre",
+        "Invoice",
+        "InvoiceLine",
+        "MediaType",
+        "Playlist",
+        "PlaylistTrack",
+        "Track",
+    ]
+
+    db_query: str = "SELECT * FROM Artist LIMIT 10;"
+    db_query_output: str = (
+        "[(1, 'AC/DC'), (2, 'Accept'), (3, 'Aerosmith'), "
+        "(4, 'Alanis Morissette'), (5, 'Alice In Chains'), "
+        "(6, 'Antônio Carlos Jobim'), (7, 'Apocalyptica'), "
+        "(8, 'Audioslave'), (9, 'BackBeat'), (10, 'Billy Cobham')]"
+    )
+
+    assert db.dialect == "sqlite"
+    assert db.get_usable_table_names() == tables
+    assert db.run(db_query) == db_query_output
+
+
+def test_query_prompt() -> None:
+    """Test query prompt content."""
+    template: str = (
+        "Given an input question, create a syntactically correct {dialect} "
+        "query to run to help find the answer. "
+        "Unless the user specifies in his question a specific number of "
+        "examples they wish to obtain, "
+        "always limit your query to at most {top_k} results. "
+        "You can order the results by a relevant column to return the most "
+        "interesting examples in the database.\n\n"
+        "Never query for all the columns from a specific table, "
+        "only ask for a the few relevant columns given the question.\n\n"
+        "Pay attention to use only the column names that you can see in the "
+        "schema description. "
+        "Be careful to not query for columns that do not exist. "
+        "Also, pay attention to which column is in which table.\n\n"
+        "Only use the following tables:\n{table_info}\n\n"
+        "Question: {input}"
+    )
+
+    assert len(query_prompt_template.messages) == 1
+    assert query_prompt_template.messages[0].prompt.template == template
+
+
+def test_write_query() -> None:
+    """Test write query."""
+    user_input: str = "How many Employees are there?"
+    expected_output: str = "SELECT COUNT(*) FROM Employee"
+
+    assert write_query({"question": user_input})["query"] == expected_output
+
+
+def test_query_execute() -> None:
+    """Test query execute."""
+    ai_input: str = "SELECT COUNT(*) FROM Employee"
+    expected_output: dict[str: Any] = {"result": "[(8,)]"}
+
+    assert execute_query({"query": ai_input}) == expected_output
